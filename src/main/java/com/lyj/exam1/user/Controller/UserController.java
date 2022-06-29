163c83ae660bc62c946da31376e893f33304c27c
leeyeonji28 <yeon102800@gmail.com>
2022-06-29 13:03:52 +0900

    ãDevTools 설치하여  Hot Reload 적용

--- src/main/java/com/lyj/exam1/user/Controller/UserController.java
+++ src/main/java/com/lyj/exam1/user/Controller/UserController.java
@@ -5,17 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/api") // 브라우저 요청
+@RequestMapping("/api/users") // 브라우저 요청
 public class UserController { // 브라우저의 요청에 반응
-    @RequestMapping("/users")
+    @RequestMapping("")
     @ResponseBody
     public String users(){
-        return "users";
+        return "users :-))";
     }
 
     @RequestMapping("1")
     @ResponseBody
     public String user(){
-        return "user";
+        return "user :(";
     }
 }
